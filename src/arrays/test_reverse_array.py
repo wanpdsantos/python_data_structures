d762426390cf6793c11f11461e6e7f3b71270a2a
Wanderson Pinto <wanpdsantos@gmail.com>
2023-01-14 11:19:20 -0300

    updated test cases

--- src/arrays/test_reverse_array.py
+++ src/arrays/test_reverse_array.py
@@ -1,15 +1,16 @@
 from .reverse_array import ReverseArray
 
+test_array = [1, 2, 3]
+test_case = ReverseArray(test_array)
+
 
 def test_reverse_array_init():
     """Check if the class is correctly initializing"""
-    test_array = [1, 2, 3]
-    test_case = ReverseArray(test_array)
+
     assert isinstance(test_case, ReverseArray)
 
 
 def test_reverse_array():
     """Test for the ordering array process after exponential calculation"""
-    test_array = [1, 2, 3]
-    test_case = ReverseArray(test_array)
+
     assert test_case.reverse() == [3, 2, 1]
