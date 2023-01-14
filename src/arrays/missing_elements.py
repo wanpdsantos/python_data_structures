978e2ef442ac56d90c318b00a82101c928b2bdab
Wanderson Pinto <wanpdsantos@gmail.com>
2023-01-14 16:31:30 -0300

    included meaningfull name

--- src/arrays/missing_elements.py
+++ src/arrays/missing_elements.py
@@ -7,13 +7,15 @@ class MissingElements:
     def check_missing_elements_from_ordered_array(self, input_array: list[int]):
         """Check for missing element in a list of ordered integers"""
 
-        difference = input_array[0]
+        difference_between_index_and_element = input_array[0]
         missing_elements = []
         for index, element in enumerate(input_array):
-            if element - index != difference:
+            if element - index != difference_between_index_and_element:
                 # required when multiple element in sequence are missing
-                for missing in range(difference, element - index):
+                for missing in range(
+                    difference_between_index_and_element, element - index
+                ):
                     missing_elements.append(missing + index)
 
-                difference = element - index
+                difference_between_index_and_element = element - index
         return missing_elements
